05f0d106da7167d9b70dced1ce4e50dd932cbf24
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2022-03-26 16:28:50 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-from DPM import Cell
-from matplotlib import pyplot as plt
+try:
+  from DPM import Cell
+except:
+  print('Please compile the module using "python3 setup.py build_ext --inplace"')
+  exit(1)
+
+try:
+  from matplotlib import pyplot as plt
+except:
+  print('This demo required matplotlib pyplot to be installed. \nPlease install with "pip install matplotlib"')
+  exit(1)
 
 C = Cell(1,0,0,1.17,50,1.0,0.1,1.0,0.1,0.1,0.2,1.0,0.0);
 
