5eb30ff1d4b697d4d49e3b8b691a95a37b9bbf2d
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2022-03-26 13:25:11 -0400

    Create Cell.hpp

---
+++ include/Cell/Cell.hpp
@@ -0,0 +1,57 @@
+#include <vector>
+namespace DPM {
+    class Cell
+    {
+        public:
+            int idx;
+            int NV;
+            double calA0;
+            double l0;
+            double r0;
+            std::vector<double> X;
+            std::vector<double> Y;
+            std::vector<double> vx;
+            std::vector<double> vy;
+            std::vector<double> Fx;
+            std::vector<double> Fy;
+            double v0;
+            double Ka;
+            double Kb;
+            double Kl;
+            std::vector<int> ip1;
+            std::vector<int> im1;
+            double vmin;
+            double Dr;
+            double Ds;
+            double a0;
+            double psi;
+            std::vector<double> l1;
+            std::vector<double> l2;
+
+
+            Cell(int idx,
+                double x1,double y1,
+                double calA0,
+                int NV, 
+                double Kl, double Kb, double Ka, 
+                double v0, double Dr, double Ds, 
+                double a0, double psi);
+            
+            void PerimeterForceUpdate();
+            void AreaForceUpdate();
+            void BendingForceUpdate();
+            void DrivingForceUpdate(double dt);
+            void UpdateShapeForces();
+            void UpdateEuler(double dt);
+            void UpdateVV();
+            void UpdateDirectorDiffusion(double dt);
+            void UpdateDirectorPsi(double newpsi);
+            void UpdateStickness(int vertex, double l1, double l2);
+            double GetPerim();
+            double GetArea();
+            double GetCenterX();
+            double GetCenterY();
+            double GetShapeParam();
+            bool isConvex();
+    };
+}
