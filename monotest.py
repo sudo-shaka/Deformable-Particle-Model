9c395a2325dae49e405b7a56e32d1e3d43880722
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2022-03-28 15:27:35 -0400

    Update monotest.py

--- monotest.py
+++ monotest.py
@@ -1,26 +1,32 @@
-import DPM
-from matplotlib import pyplot as plt
-from progressbar import progressbar
-import imageio
+try:
+  import DPM
+  from matplotlib import pyplot as plt
+  from progressbar import progressbar
+  import imageio
+except:
+  print("You do not have the required dependencies to run this simulation");
 
 def Euler():
+  #initialize a cell with calA of 1.4, 32 vertecies, spring and director constants plus an area of 20.
   C = DPM.Cell(1,0,0,1.4,32,1.0,0.5,0.1,0.05,0.2,0.1,20.0,0)
-#  C.l1 = [0.1]*32;
-#  C.l2 = [0.05]*32;
-#  C1.l1 = [0.1]*28;
-#  C1.l2 = [0.05]*28;
-
 
   ncells = 7;
+  #initialize a monolayer if n number cells and a packing fraction of 
   mono = DPM.monolayer([C]*ncells,0.85);
+  #Disperse the monolayer using simple hard sphere repulsions
   mono.disperse();
   count = 0;
+  #Set the spring constant for cell-cell interactions
   mono.Kc = 0.5;
-
+  
+  #begining time steps
   for i in progressbar(range(int(20000))):
+    #for every cell change the direction its going
     for ci in range(ncells):
       mono.Cells[ci].UpdateDirectorDiffusion(0.001)
+    #update the positions with a dt of 0.001
     mono.UpdateEuler(0.001);
+    #plot every 1000 time steps
     if i % 1000 == 0:
       for ci in range(ncells):
         plt.scatter(mono.Cells[ci].X,mono.Cells[ci].Y)
@@ -28,12 +34,15 @@ def Euler():
       plt.savefig('/tmp/'+str(count)+'.png')
       plt.clf();
       count +=1;
-
+  
+  #this is to simply save the image
   with imageio.get_writer('/tmp/out.gif',mode='I') as writer:
     for i in range(count):
       filename = '/tmp/'+str(i)+'.png'
       image = imageio.imread(filename)
       writer.append_data(image)
+  #return the monolayer
+  print('Image is saved to "/tmp/out.gif"')
   return mono
 
 
