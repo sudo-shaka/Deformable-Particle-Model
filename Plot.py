98582b967d2893402628fbb67ed81da0836cfabd
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2022-03-28 18:08:15 -0400

    Add files via upload

---
+++ Plot.py
@@ -0,0 +1,25 @@
+import DPM
+from matplotlib import pyplot as plt
+
+
+def PlotDPM(monolayer):
+    L = monolayer.BoxLength;
+    for ci in range(monolayer.NCELLS):
+        X = []; Y = [];
+        for vi in range(len(monolayer.Cells[ci].X)):
+            x = monolayer.Cells[ci].X[vi]
+            y = monolayer.Cells[ci].Y[vi]
+            if monolayer.Cells[ci].X[vi] < 0:
+                x = monolayer.Cells[ci].X[vi] + L*(int(abs(monolayer.Cells[ci].X[vi])/L)+1)
+            if monolayer.Cells[ci].X[vi] > L:
+                x = monolayer.Cells[ci].X[vi] - L*(int(abs(monolayer.Cells[ci].X[vi]-L)/L)+1)
+            if monolayer.Cells[ci].Y[vi] < 0:
+                y = monolayer.Cells[ci].Y[vi] + L*(int(abs(monolayer.Cells[ci].Y[vi])/L)+1)
+            if monolayer.Cells[ci].Y[vi] > L:
+                y = monolayer.Cells[ci].Y[vi] - L*(int(abs(monolayer.Cells[ci].Y[vi]-L)/L)+1)
+            X.append(x)
+            Y.append(y)
+        plt.scatter(X,Y);
+
+    plt.axis('equal')
+    #plt.show()
