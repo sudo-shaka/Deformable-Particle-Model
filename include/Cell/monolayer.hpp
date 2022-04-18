74e254b408dd4bfbd7e478bfd4a409f1cb462ed9
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2022-04-18 00:02:02 +0000

    Add files via upload

--- include/Cell/monolayer.hpp
+++ include/Cell/monolayer.hpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <Cell.hpp>
+#include <functional>
 namespace DPM{
     class monolayer{
         public:
@@ -12,26 +13,27 @@ namespace DPM{
             int VertDOF;
             double L;
             double Kc;
-            double Ftol;
-            double dt0;
             double U;
-            bool pinning;
             std::vector<DPM::Cell> Cells;
             std::vector<bool> overlaps;
+            int cellidx = 0;
              //functions
             void disperse();
-            void VertexFIRE();
-            void UpdateEuler(int nsteps, double dt);
-            void UpdateVV(int nsteps, double dt);
+            void VertexFIRE(std::function<void()> InteractingMethod, double alpha, double dt, int maxits, double Ftol);
+            void UpdateEuler(std::function<void()> InteractingMethod,int nsteps, double dt);
+            void UpdateEuler(double dt);
+            void UpdateVV(std::function<void()> InteractingMethod, int nsteps, double dt);
             double GetPackingFraction();
             double GetVertexKineticEnergy();
             void FindOverlaps(int ci, int cj);
-            void InteractingForceUpdate();
+            void RepulsiveForces();
+            void AttactiveForces();
+            void PinnedForces();
+            void MixedInteractingMethods(std::vector<bool> firstMethod,std::function<void()> Method1, std::function<void()> Method2);
+            void ShapeForceUpdate();
             void ResetForces();
             void CellDivision(int cellidx);
-            void FindNearestNeighbor();
-            void StartPinning();
-            void StopPinning();
+            void NearestNeighborUpdate();
             void FindNearestNeighbor(int ci);
     };
 }
